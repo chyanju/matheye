c90eebe9d9982c2d04aea9b810e68553224e6a74
Yanju Chen <chyanju@gmail.com>
2025-05-06 21:04:40 -0700

    Update demo3.lean

--- benchmarks/minif2f/demos/demo3.lean
+++ benchmarks/minif2f/demos/demo3.lean
@@ -4,7 +4,7 @@ set_option maxHeartbeats 0
 open BigOperators Real Nat Topology Rat
 macro "hole" : tactic => `(tactic| admit)
 
--- benchmarks/minif2f-dspv2/test/mathd_numbertheory_185.lean
+-- benchmarks/minif2f-dspv2/test/mathd_numbertheory_222.lean
 -- the previous goal h₁ can be directly solved by "norm_num"
 example (b : ℕ)
   (h₀ : Nat.lcm 120 b = 3720)
